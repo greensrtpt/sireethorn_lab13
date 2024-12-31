40e28f4d49a86e27cc16a83429a56b295c604bf2
sireethorn <sireethorn2548.h20p@gmail.com>
2024-12-31 14:58:26 +0700

    lab13_1(Compile).cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,26 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double data[], int size, double results[]) {
+    double sum = 0.0, sumSq = 0.0, product = 1.0, harmonicSum = 0.0;
+    double maxVal = numeric_limits<double>::min();
+    double minVal = numeric_limits<double>::max();
+
+    for (int i = 0; i < size; i++) {
+        double value = data[i];
+        sum += value;
+        sumSq += value * value;
+        product *= value;
+        harmonicSum += 1.0 / value;
+        if (value > maxVal) maxVal = value;
+        if (value < minVal) minVal = value;
+    }
+
+    double mean = sum / size;
+    results[0] = mean;
+    results[1] = sqrt(sumSq / size - mean * mean);
+    results[2] = pow(product, 1.0 / size);
+    results[3] = size / harmonicSum;
+    results[4] = maxVal;
+    results[5] = minVal;
+}
